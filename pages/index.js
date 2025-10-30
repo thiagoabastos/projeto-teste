42f9326d2178edd2d6e5cc497abb42ecb817cb45
thiago_bastos <ticobtj@gmail.com>
2025-10-29 21:47:41 -0300

    vercel blob

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-function Home() {
+import { put } from "@vercel/blob";
+
+async function Home() {
+    const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
+console.log('Blob URL:', url);
     return (
         <div>
             <h1>Home 5</h1>
